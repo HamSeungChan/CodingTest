c34b2a0de6692e15f9a8373346749b6bc233d50d
HamSeungChan <head0618@naver.com>
2023-03-03 17:29:46 +0900

    프로그래ë머스 x만큼 간격이 있는 n개의 숫자

--- src/main/java/programmers/com/ll/level1/p12954/Main.java
+++ src/main/java/programmers/com/ll/level1/p12954/Main.java
@@ -1,16 +1,22 @@
 package programmers.com.ll.level1.p12954;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();   //x는 -10000000 이상, 10000000 이하인 정수입니다.
+        int n = sc.nextInt();   //n은 1000 이하인 자연수입니다.
     }
 }
 
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
-        for(int i=0;i<n;i++){
-            answer[i] = x * (i+1);
+        long lx =x ;
+        for (int i = 0; i < n; i++) {
+            answer[i] = lx;
+            lx += x;
         }
         return answer;
     }
