ff342b13fd94b969536c56b3f3a3fc12c5181f2e
HamSeungChan <head0618@naver.com>
2023-03-05 22:03:38 +0900

    프로그래머스 문자열을 정수로 바꾸기

---
+++ src/main/java/programmers/com/ll/level1/p12925/Main.java
@@ -0,0 +1,18 @@
+package programmers.com.ll.level1.p12925;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int i = new Solution().solution("-1234");
+        System.out.println(i);
+    }
+}
+
+class Solution {
+    public int solution(String s) {
+
+
+        return Integer.valueOf(s);
+    }
+}
