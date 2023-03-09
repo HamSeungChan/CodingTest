f600614f4287148277d45e89452ca749c146fd46
HamSeungChan <head0618@naver.com>
2023-03-09 17:29:16 +0900

    문자열 내림차순으로 배치하기

---
+++ src/main/java/programmers/com/ll/level1/p12917/Main.java
@@ -0,0 +1,32 @@
+package programmers.com.ll.level1.p12917;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public String solution(String s) {
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        StringBuilder sb = new StringBuilder(String.valueOf(c)).reverse();
+        return sb.toString();
+    }
+}
+
+class OtherSolution {
+
+    public String solution(String s) {
+        return Stream.of(s.split(""))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining());
+    }
+}
+
+
